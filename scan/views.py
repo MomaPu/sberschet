0a0c9ac3bef4f8fef670c2e6784c272e50270912
Arseniy <y4ne242@gmail.com>
2025-04-17 20:49:26 +0300

    Функция сканирования

--- scan/views.py
+++ scan/views.py
@@ -1,32 +1,73 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
 from .models import Hotel, User
 from .forms import HotelForm, HomepageForm, ProfileForm
 from PIL import Image
 import io
 import base64
-#from apple_ocr.ocr import OCR
+from apple_ocr.ocr import OCR
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+KEYWORDS = ['сумма', 'итог', 'загальна', 'всего', 'total', 'sum', 'amount', 'due', 'balance', 'итого к оплате:', 'к оплате']
 
 def hotel_image_view(request):
+    context = {}
     if request.method == 'POST':
         form = HotelForm(request.POST, request.FILES)
         if form.is_valid():
             hotel_instance = form.save(commit=False)
-
-            # Сохраняем изображение
+            
             image_file = request.FILES['hotel_Main_Img']
             img = Image.open(image_file)
-
-            # Можно сохранить сразу файл или обработать
-            # Для сохранения в модель
+            
+            ocr_instance = OCR(image=img)
+            data = ocr_instance.recognize()
+            
+            for parts in data['Content']:
+                if str(parts).lower() in KEYWORDS:
+                    index = list(data['Content']).index(str(parts))
+            price_1 = str(data['Content'][index+1]).replace(",", ".", 1)
+            price_1 = price_1.replace(" ", "")
+            price_1 = price_1.replace("Руб.", "")
+            price_1 = price_1.replace("=", "")
+            
+            price_2 = str(data['Content'][index-1]).replace(",", ".", 1)
+            price_2 = price_2.replace(" ", "")
+            price_2 = price_2.replace("Руб.", "")
+            price_2 = price_2.replace("=", "")
+            
+            price_3 = str(data['Content'][-1]).replace(",", ".", 1)
+            price_3 = price_3.replace(" ", "")
+            price_3 = price_3.replace("Руб.", "")
+            price_3 = price_3.replace("=", "")
+            
+            try:
+                price_1 = float(price_1)
+            except:
+                price_1 = 0
+            try:
+                price_2 = float(price_2)
+            except:
+                price_2 = 0
+            try:
+                price_3 = float(price_3)
+            except:
+                price_3 = 0
+            
+            price = max(price_3, price_1, price_2)
+                        
+            context['ocr_result'] = price
+            
             hotel_instance.hotel_Main_Img = image_file
             hotel_instance.save()
 
-            return redirect('../success')  # замените на ваш URL
+            context['form'] = form
+            return render(request, 'scaner.html', context)
     else:
         form = HotelForm()
-    return render(request, 'scaner.html', {'form': form})
-
+    
+    context['form'] = form
+    return render(request, 'scaner.html', context)
 
 def success(request):
     return HttpResponse('successfully uploaded')
